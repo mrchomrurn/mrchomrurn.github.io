03c314937f66d4830ef250dc05ea48ddd1830974
Chomrurn Maing <45104633+mrchomrurn@users.noreply.github.com>
2023-08-19 16:54:46 +0700

    config: deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default ({ mode }) => {
   return defineConfig({
-    base: mode === 'development' ? '/' : '/mrchomrurn.github.io/',
+    base: '/mrchomrurn.github.io/',
     plugins: [vue()],
   })
 }
