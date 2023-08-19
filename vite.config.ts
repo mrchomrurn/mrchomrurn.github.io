7d4a7d8bfc4bcbaad4ed98be5bb8fff49358f47d
Chomrurn Maing <45104633+mrchomrurn@users.noreply.github.com>
2023-08-19 16:45:15 +0700

    config: deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default ({ mode }) => {
+  return defineConfig({
+    base: mode === 'development' ? '/' : '/mrchomrurn.github.io/',
+    plugins: [vue()],
+  })
+}
