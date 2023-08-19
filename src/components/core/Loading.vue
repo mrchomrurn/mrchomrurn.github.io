41d68a6618409768717bfc6451d283cd8680a29f
Chomrurn Maing <45104633+mrchomrurn@users.noreply.github.com>
2023-08-19 16:10:19 +0700

    feat: loading component

---
+++ src/components/core/Loading.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="d-flex align-items-center justify-content-center w-100 h-100">
+    <InlineSvg src="/icons/circular-spinner.svg"></InlineSvg>
+  </div>
+</template>
+
+<script setup lang="ts">
+import InlineSvg from 'vue-inline-svg'
+</script>
