19a9fb36b6ff4fccb2fdc94a7fa3dab4619bb727
Chomrurn Maing <45104633+mrchomrurn@users.noreply.github.com>
2023-08-19 12:59:36 +0700

    ref: add aditional columns

--- src/components/CountryListing.vue
+++ src/components/CountryListing.vue
@@ -11,16 +11,19 @@
       class="col-sm-6 col-md-4 col-lg-3 mb-2"
       :key="`country-${country.name}`"
     >
-      <div class="bg-white shadow rounded-3 overflow-hidden text-center p-2">
+      <div class="bg-white shadow rounded-3 overflow-hidden p-2">
         <img
           :src="country.flags.png"
           class="img-fluid flag mb-2 rounded shadow-sm"
           alt=""
         />
         <div class="p-3">
-          <h6 class="text-ellipsis">{{ country.name.official }}</h6>
-          <p class="mb-0 text-muted">Native Name</p>
-          <div class="d-flex overflow-auto flex-nowrap">
+          <h6 class="text-ellipsis mb-3">{{ country.name.official }}</h6>
+          <p class="mb-0 text-muted">Native Name:</p>
+          <div
+            class="d-flex overflow-auto flex-nowrap mb-3"
+            style="height: 24px"
+          >
             <div
               v-for="(name, key) in country.name.nativeName"
               :key="`native-name-${key}`"
@@ -30,11 +33,23 @@
               {{ name.official }}
             </div>
           </div>
+
+          <p class="mb-0 text-muted">Alternative Name:</p>
+          <p class="mb-3 text-ellipsis">
+            {{ country.altSpellings.join(', ') }}
+          </p>
+
           <p class="mb-0 text-muted">
             CODE 2: <span class="fw-bold text-dark">{{ country.cca2 }}</span>
           </p>
           <p class="mb-0 text-muted">
             CODE 3: <span class="fw-bold text-dark">{{ country.cca3 }}</span>
+          </p>
+          <p class="mb-0 text-muted">
+            Calling Codes:
+            <span class="fw-bold text-dark">{{
+              `${country.idd.root}${country.idd.suffixes}`
+            }}</span>
           </p>
         </div>
       </div>
