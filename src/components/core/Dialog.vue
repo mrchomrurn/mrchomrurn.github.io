49148706c021f35d382a49b8bc3caa4b3db02198
Chomrurn Maing <45104633+mrchomrurn@users.noreply.github.com>
2023-08-19 16:09:37 +0700

    style: dialog header style

--- src/components/core/Dialog.vue
+++ src/components/core/Dialog.vue
@@ -8,7 +8,7 @@
   <div class="modal fade" :id="id" aria-hidden="true" tabindex="-1">
     <div class="modal-dialog modal-fullscreen">
       <div class="modal-content">
-        <div class="modal-header">
+        <div class="modal-header border-bottom-lg">
           <h1 class="modal-title fs-5" id="exampleModalToggleLabel">
             {{ title }}
           </h1>
